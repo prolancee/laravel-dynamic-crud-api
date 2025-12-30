50d829809bd6e24b2a388d12fe0b870ed2271d43
prolancee <salim.shaikh.dev@gmail.com>
2025-12-30 22:21:34 +0530

    fix: resolve service provider binding issue

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -2,12 +2,13 @@
 
 namespace PROLANCEE\DYNAMIC\CRUD\Api\Providers;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use PROLANCEE\DYNAMIC\CRUD\Api\App\Http\Middleware\{
     ValidateIntermediateRoutes,
     ForceJsonResponse,
